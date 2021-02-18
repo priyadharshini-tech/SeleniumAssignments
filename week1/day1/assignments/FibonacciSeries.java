48d2270a476528a6e289089accd23c5ddb11c180
priyadharshini-tech <79267991+priyadharshini-tech@users.noreply.github.com>
2021-02-18 20:52:10 +0530

    Update FibonacciSeries.java

--- week1/day1/assignments/FibonacciSeries.java
+++ week1/day1/assignments/FibonacciSeries.java
@@ -18,18 +18,20 @@ public class FibonacciSeries {
 	public static void main(String[] args) {
 
 		// initialize 3 int variables (Tip: range = 8, firstNum = 0, secNum = 1, sum in the series)
-
+                int n1=0,n2=1;i,count=10;
 		// Print first number
-		
+		System.out.print(n1+""+n2);
 		// Iterate from 1 to the range
-		
+		for(i=2;i<count;++i);{
 		// add first and second number assign to sum
-
+                n3=n1+n2;
 		// Assign second number to the first number
-
+                n1=n2;
 		// Assign first number to the sum
-		
+		n2=n3;
 		// print sum
+                System.out.println(""+n3);
+                }
 
 	}
 
